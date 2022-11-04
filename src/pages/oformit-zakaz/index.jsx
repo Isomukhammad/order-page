b68eb7028ac9ec0f70f82d3eb2f3793375af9774
Isomukhammad <s.isomuxammad@gmail.com>
2022-11-04 21:40:01 +0500

    Fixing overflow

--- src/pages/oformit-zakaz/index.jsx
+++ src/pages/oformit-zakaz/index.jsx
@@ -3,11 +3,11 @@ import Sidebar from "../../components/sidebar/sidebar.component";
 import styles from './oformit-zakaz.module.scss';
 
 const OformitZakaz = () => (
-    <div className = {styles.container}> 
-        <Sidebar>
-            Hello
-        </Sidebar>
-    </div>
+    <Sidebar>
+        <div className = {styles.container}> 
+            <div>Hello</div>
+        </div>
+    </Sidebar>
 )
 
 export default OformitZakaz;
