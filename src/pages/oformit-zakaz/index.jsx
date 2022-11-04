9cc08eb07d3d76e735cd1fddf05e72b14ed1c1eb
Isomukhammad <s.isomuxammad@gmail.com>
2022-11-04 22:24:37 +0500

    Fixing width

--- src/pages/oformit-zakaz/index.jsx
+++ src/pages/oformit-zakaz/index.jsx
@@ -5,7 +5,9 @@ import styles from './oformit-zakaz.module.scss';
 const OformitZakaz = () => (
     <Sidebar>
         <div className = {styles.container}> 
-            <div>Hello</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolor dicta tempore sed, nam consectetur distinctio voluptas porro harum quibusdam architecto expedita nesciunt et rerum esse ab cumque eius iusto.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis minima amet fugiat, aperiam, ducimus sunt cupiditate consequatur hic alias debitis quia. Voluptatum unde nihil velit fugit quos repellat autem iure? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit doloremque esse, quasi in quos dicta id! Blanditiis aspernatur quia quos beatae odit. Explicabo rem odit iusto autem est culpa consequuntur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, quam saepe, numquam quae neque soluta tempore enim eaque ex hic laborum! Ipsum blanditiis magni omnis et quo fuga ab quas!
+            </div>
         </div>
     </Sidebar>
 )
